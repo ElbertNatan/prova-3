97aaa521282d34b2552b675e6dc7fad16743ece3
elbertnatan <elbertnatan@gmail.com>
2025-01-16 16:08:25 -0300

    Feature: botão para deslogar;

--- app/src/main/java/com/example/imdlibrary/MenuActivity.kt
+++ app/src/main/java/com/example/imdlibrary/MenuActivity.kt
@@ -30,5 +30,13 @@ class MenuActivity : ComponentActivity() {
         binding.listButton.setOnClickListener {
             startActivity(Intent(this, ListActivity::class.java))
         }
+
+        // Botão para deslogar
+        binding.logoutButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
     }
 }
